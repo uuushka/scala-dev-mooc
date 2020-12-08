cb768bbf61d0931180df5c710445270066194b25
uuushka <Yushka1988#>
2020-12-08 09:31:21 +0300

    lesson2 add Option and List methods

--- src/main/scala/module1/02-fp.scala
+++ src/main/scala/module1/02-fp.scala
@@ -1,192 +1,253 @@
 package module1
 
-import module1.list.List.Cons
+import module1.list.List.{::, Cons, Nil}
 
 import scala.annotation.tailrec
 
 /**
- *  Реализуем тип Option
+ * Реализуем тип Option
  */
 
 
- object opt {
+object opt {
 
   /**
    *
    * Реализовать тип Option, который будет указывать на присутствие либо отсутсвие результата
    */
 
-   sealed trait Option[+A]{
-      def isEmpty: Boolean = this match {
-       case Option.Some(_) => false
-       case Option.None => true
-      }
+  sealed trait Option[+A] {
 
-      def get: A = this match {
-       case Option.Some(v) => v
-       case Option.None => throw new Exception("get on empty Option")
-      }
-   }
+    import Option.{Some, None}
 
-   object Option {
+    def isEmpty: Boolean = this match {
+      case Some(_) => false
+      case None => true
+    }
+
+    def get[A]: A = this match {
+      case Some(v: A) => v
+      case None => throw new Exception("get on empty Option")
+    }
+
+    /**
+     *
+     * Реализовать метод printIfAny, который будет печатать значение, если оно есть
+     */
+    def printIfAny: Unit = this match {
+      case Some(v) => println(v)
+      case None => println(None)
+    }
+
+    /**
+     *
+     * реализовать метод orElse который будет возвращать другой Option, если данный пустой
+     */
+    def orElse[A](altV: A): A = this match {
+      case Some(v: A) => v
+      case None => altV
+    }
+
+    /**
+     *
+     * Реализовать метод zip, который будет создавать Option от пары значений из 2-х Option
+     */
+    def zip[A](optV: Option[A]): Option[(A, A)] = this match {
+      case None => None
+      case Some(v: A) => {
+        optV match {
+          case Some(optV: A) => Option[(A, A)]((v, optV))
+          case None => None
+        }
+      }
+    }
+
+    /**
+     *
+     * Реализовать метод filter, который будет возвращать не пустой Option
+     * в случае если исходный не пуст и предикат от значения = true
+     */
+    def filter(pred: A => Boolean): Option[A] = {
+      // тут сделаю без pattern matching
+      if (this.isEmpty) None
+      else if (pred(get)) this else None
+    }
+
+  }
+
+  object Option {
+
     case class Some[A](v: A) extends Option[A]
+
     case object None extends Option[Nothing]
-   }
 
+    def apply[A](v: A): Option[A] = v match {
+      case None => None
+      case _ => Some(v)
+    }
 
+  }
 
+}
 
-  /**
-   *
-   * Реализовать метод printIfAny, который будет печатать значение, если оно есть
-   */
-
-  /**
-   *
-   * реализовать метод orElse который будет возвращать другой Option, если данный пустой
-   */
-
+object recursion {
 
   /**
-   *
-   * Реализовать метод isEmpty, который будет возвращать true если Option не пуст и false в противном случае
+   * Реализовать метод вычисления n!
+   * n! = 1 * 2 * ... n
    */
 
-
-  /**
-   *
-   * Реализовать метод get, который будет возвращать значение
-   */
-
-  /**
-   *
-   * Реализовать метод zip, который будет создавать Option от пары значений из 2-х Option
-   */
-
-
-  /**
-   *
-   * Реализовать метод filter, который будет возвращать не пустой Option
-   * в случае если исходный не пуст и предикат от значения = true
-   */
-
- }
-
- object recursion {
-
-   /**
-    * Реализовать метод вычисления n!
-    * n! = 1 * 2 * ... n
-    */
-
-   def fact(n: Int): Long = {
+  def fact(n: Int): Long = {
     var _n = 1L
     var i = 2
     while (i <= n) {
-     _n *= i
-     i += 1
+      _n *= i
+      i += 1
     }
     _n
-   }
-
-   def !!(n: Int): Long = {
-     if(n <= 1) 1
-     else n * !!(n - 1)
-   }
-
-  def !(n: Int): Long = {
-   @tailrec
-   def loop(n1: Int, acc: Long): Long = {
-     if(n <= 1) acc
-     else loop(n1 - 1, n1 * acc)
-    }
-   loop(n, 1)
   }
 
- }
+  def !!(n: Int): Long = {
+    if (n <= 1) 1
+    else n * !!(n - 1)
+  }
 
- object list {
-   /**
-    *
-    * Реализовать односвязанный имутабельный список List
-    */
+  def !(n: Int): Long = {
+    @tailrec
+    def loop(n1: Int, acc: Long): Long = {
+      if (n <= 1) acc
+      else loop(n1 - 1, n1 * acc)
+    }
 
-   sealed trait List[+A]{
-     def ::[AA >: A](head: AA): List[AA] = Cons(head, this)
+    loop(n, 1)
+  }
+
+}
+
+object list {
+
+  /**
+   *
+   * Реализовать односвязанный имутабельный список List
+   */
+
+  sealed trait List[+A] {
+
+    import List._
+
+    def ::[AA >: A](head: AA): List[AA] = Cons(head, this)
 
     def mkString: String = mkString(", ")
 
     def mkString(sep: String): String = {
-       import List._
 
       def loop(l: List[A], acc: StringBuilder): StringBuilder = {
         l match {
-         case List.Nil => acc
-         case h :: Nil => acc.append(s"$h")
-         case h :: t => loop(t, acc.append(s"$h$sep"))
+          case List.Nil => acc
+          case h :: Nil => acc.append(s"$h")
+          case h :: t => loop(t, acc.append(s"$h$sep"))
         }
-       }
-      loop(this, new StringBuilder()).toString()
-     }
-   }
+      }
 
-   object List{
+      loop(this, new StringBuilder()).toString()
+    }
+
+    /**
+     *
+     * Реализовать метод reverse который позволит заменить порядок элементов в списке на противоположный
+     */
+    def reverse: List[A] = {
+      def loop(l: List[A], reverseList: List[A] = Nil): List[A] = {
+        l match {
+          case Nil => reverseList
+          case head :: tail => loop(tail, head :: reverseList)
+        }
+      }
+
+      loop(this, List.Nil)
+    }
+
+   /**
+     *
+     * Написать функцию incList котрая будет принимать список Int и возвращать список,
+     * где каждый элемент будет увеличен на 1
+     */
+    def incList:List[Int] = incList(1)
+
+    def incList(add: Int): List[Int] = {
+      def loop(l: List[A], incL: List[Int]): List[Int] = {
+        l match {
+          case Nil => incL
+          case head :: tail => loop(tail, (head.asInstanceOf[Int]+add) :: incL)
+        }
+      }
+      try {
+        loop(this, Nil).reverse
+      } catch {
+        case e: ClassCastException => {
+          println("Not cast type A to Int")
+          Nil
+        }
+      }
+    }
+
+    /**
+     *
+     * Написать функцию shoutString котрая будет принимать список String и возвращать список,
+     * где к каждому элементу будет добавлен префикс в виде '!'
+     */
+
+    def shoutString: List[String] = shoutString("!")
+    def shoutString(prefix:String): List[String] = {
+      def loop(l: List[A], shoutList: List[String]): List[String] = {
+        l match {
+          case Nil => shoutList
+          case head :: tail => loop(tail, s"$prefix$head":: shoutList)
+        }
+      }
+
+      loop(this, Nil).reverse
+    }
+
+    /**
+     *
+     * Реализовать метод для списка который будет применять некую ф-цию к элементам данного списка
+     */
+    def mapF[AA >: A](f: AA => AA): List[AA] = {
+      def loop(l: List[AA], mapList: List[AA]): List[AA] = {
+        l match {
+          case Nil => mapList
+          case head :: tail => loop(tail, f(head):: mapList)
+        }
+      }
+
+      loop(this, Nil).reverse
+    }
+
+  }
+
+  object List {
+
     case object Nil extends List[Nothing]
+
     case class ::[A](head: A, tail: List[A]) extends List[A]
+
     val Cons = ::
 
-    def apply[T](arg: T*): List[T] = {
-     var l: List[T] = List.Nil
-     arg.foreach(el => l = el :: l)
-     l
+    def apply[T](args: T*): List[T] = {
+      var l: List[T] = List.Nil
+      args.foreach(el => l = el :: l)
+      l.reverse
     }
-   }
-
-   val list = 1 :: List.Nil
-
-   /**
-    *
-    * Реализовать метод конс :: который позволит добавлять элемент в голову списка
-    */
+  }
 
 
-   /**
-    *
-    * Реализовать конструктор, для создания списка n элементов
-    */
-
-
-   /**
-    *
-    * Реализовать метод mkString который позволит красиво представить список в виде строки
-    */
-
-
-   /**
-    *
-    * Реализовать метод reverse который позволит заменить порядок элементов в списке на противоположный
-    */
-
-
-   /**
-    *
-    * Написать функцию incList котрая будет принимать список Int и возвращать список,
-    * где каждый элемент будет увеличен на 1
-    */
-
-
-   /**
-    *
-    * Написать функцию shoutString котрая будет принимать список String и возвращать список,
-    * где к каждому элементу будет добавлен префикс в виде '!'
-    */
-
-
-   /**
-    *
-    * Реализовать метод для списка который будет применять некую ф-цию к элементам данного списка
-    */
+  val list = 1 :: List.Nil
 
 
 
- }
+
+
+
+
+}
--- src/main/scala/module1/App.scala
+++ src/main/scala/module1/App.scala
@@ -1,18 +1,55 @@
 package module1
 
 import module1.list.List
-
-
+import module1.opt.Option
 
 
 object App {
 
 
   def main(args: Array[String]): Unit = {
-
-    val list = 1 :: 2 :: 3 :: List.Nil
+    println("//--------------------LIST---------------//")
+    val list: List[Int] = 1 :: 2 :: 3 :: List.Nil
     val list2 = List(1, 2, 3)
-    println(list.mkString)
-    println(list2.mkString(" "))
+    val list3 = List("a", "b", "c")
+    println("mkString= " + list.mkString)
+    println("mkString with sep = " + list2.mkString(" "))
+
+    println("reverse= " + list.reverse.mkString)
+
+    println("incList on Int= " + list.incList.mkString)
+    println("incList on String= " + list3.incList.mkString)
+    println("incList on Int with param = " + list.incList(100).mkString)
+
+    println("shoutString on Int List with param= " + list.shoutString("el=").mkString)
+    println("shoutString on String with default= " + list3.shoutString.mkString)
+
+
+    println("mapF on Int List= " + list.mapF(x => x * x).mkString)
+    println("mapF on String List= " + list3.mapF(x => x.repeat(2)).mkString)
+    println("//---------------------------------------//")
+    println()
+
+    //--------------------OPTION-------------//
+    val withValue: Option[Int] = Option(3)
+    val emptyOption: Option[Int] = Option.None
+
+    print("withValue.printIfAny = ")
+    withValue.printIfAny
+    print("emptyOption.printIfAny = ")
+    emptyOption.printIfAny
+
+    println("withValue.orElse = " + withValue.orElse(-1))
+    println("emptyOption.orElse = " + emptyOption.orElse(-1))
+
+    println("withValue.zip = " + withValue.zip(Option(2)))
+    println("withValue.zip(Option(2).zip()) = " + withValue.zip(Option(2).zip(Option(1))))
+    println("emptyOption.zip = " + emptyOption.zip(Option(1)))
+
+    println("withValue.filter true = " + withValue.filter(x => x > 2))
+    println("withValue.filter false = " + withValue.filter(x => x < 2))
+    println("emptyOption.filter = " + emptyOption.filter(x => x < 2))
+
+
   }
 }
